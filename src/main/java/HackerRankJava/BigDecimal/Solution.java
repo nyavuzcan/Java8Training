67b14669189dad7a24a455b4cf179f3a4fb5fb6e
Nevzat YAVUZCAN <nevzat.yavuzcan@etiya.com>
2020-05-20 04:42:22 +0300

    Sort Bigdecimal with string

---
+++ src/main/java/HackerRankJava/BigDecimal/Solution.java
@@ -0,0 +1,27 @@
+package HackerRankJava.BigDecimal;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution {
+  public static void main(String[] args) {
+
+    Comparator<String> customComparator = new Comparator<String>() {
+      @Override
+      public int compare(String s1, String s2) {
+        BigDecimal a = new BigDecimal(s1);
+        BigDecimal b = new BigDecimal(s2);
+        return b.compareTo(a); // descending order
+      }
+    };
+
+ //  Arrays.sort(s, 0, n, customComparator);
+
+    //Output
+   /* for (int i = 0; i < n; i++) {
+      System.out.println(s[i]);
+    }
+*/
+  }
+}
